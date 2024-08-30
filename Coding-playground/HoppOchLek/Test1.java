111f13a7331696b09784736db526d53a9ad48f5a
ThereseBulai <therese.bulai@gmail.com>
2024-08-30 15:57:59 +0200

    gjort ett litet program under lektion

---
+++ Coding-playground/HoppOchLek/Test1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        /*int x = 5;
+        int y = 10;
+        int sum = x + y; 
+       
+        System.out.println(sum); */
+    
+        Scanner s = new Scanner(System.in);
+        System.out.println("Insert nickname");
+
+        String username = s.nextLine();
+        System.out.println("Hello " + username + " how old are you?");
+
+        int age = s.nextInt();
+        
+        if (age < 20) {
+            System.out.println("you are too young to enter");
+        }
+
+        if (age > 20) {
+            System.out.println("Welcome!");
+        }
+    
+    }
+    
+}
