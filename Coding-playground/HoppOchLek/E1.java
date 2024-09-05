e374122e6dd5a7e17e8c924bcfbb0f04df935223
ThereseBulai <therese.bulai@gmail.com>
2024-09-05 14:00:08 +0200

    created a new file for E1

---
+++ Coding-playground/HoppOchLek/E1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class E1 {
+    public static void main(String[] args) {
+
+        int value = 7;
+
+        if (value % 2 == 0) {
+            System.out.println("Even");
+
+        } else {
+            System.out.println("Odd");
+
+        }
+
+    }
+
+}
