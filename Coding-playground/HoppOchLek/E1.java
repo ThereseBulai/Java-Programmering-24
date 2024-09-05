ae78623be515c3793fe3f9411d2bcfe41ec5b159
ThereseBulai <therese.bulai@gmail.com>
2024-09-05 15:01:16 +0200

    added övning 1.15

--- Coding-playground/HoppOchLek/E1.java
+++ Coding-playground/HoppOchLek/E1.java
@@ -3,17 +3,29 @@ import java.util.Scanner;
 
 public class E1 {
     public static void main(String[] args) {
-
-        int value = 7;
+/*    //övning 1.14
+        
+        int value = 33;
 
         if (value % 2 == 0) {
             System.out.println("Even");
-
-        } else {
+        } 
+        else {
             System.out.println("Odd");
-
         }
+        if (value > 10);{
+            System.out.println("Greater than 10");
+        }
+ */
 
-    }
+ //Övning 1.15
+
+        String sentence = "Many things of the world is round";
+        System.out.println(sentence.indexOf("the"));
+        int index = sentence.indexOf("the");
+
+        System.out.println("The word was found at index:" + index);
+        }
+    
 
 }
