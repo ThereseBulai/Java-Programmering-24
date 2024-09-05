3119a65c51e2b51f9c9ece220c2357abc1ee9924
ThereseBulai <therese.bulai@gmail.com>
2024-09-05 15:32:05 +0200

    finished exersise

--- Coding-playground/HoppOchLek/E1.java
+++ Coding-playground/HoppOchLek/E1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class E1 {
     public static void main(String[] args) {
-/*    //övning 1.14
+/* //övning 1.14
         
         int value = 33;
 
@@ -18,14 +18,23 @@ public class E1 {
         }
  */
 
- //Övning 1.15
+
+
+ /* //Övning 1.15
 
         String sentence = "Many things of the world is round";
         System.out.println(sentence.indexOf("a"));
         int index = sentence.indexOf("a");
 
         System.out.println("The word was found at index:" + index);
-        }
+ */     
+
+ //övning 1.16
+    int a = 2, b = 4, c = 6, temp = a;
+    System.out.println("A= " + (a=b) + " B= " + (b=c) + " C= " + (c=temp));
+
+
+    }
     
 
 }
