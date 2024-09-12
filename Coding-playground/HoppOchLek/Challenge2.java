669dee9b4c045c6d9c2990edb2fa5336e3ab5893
ThereseBulai <therese.bulai@gmail.com>
2024-09-12 11:44:59 +0200

    finished multiplication table

---
+++ Coding-playground/HoppOchLek/Challenge2.java
@@ -0,0 +1,23 @@
+public class Challenge2 {
+    public static void main(String[] args) {
+    
+       //nested loop multiplication table
+ /* 
+        for (int i = 1; i <= 10; i++) {
+        
+            for (int j = 1; j <= 10; j++) {
+                
+                System.out.print(i * j + " "); 
+
+            }
+            System.out.println();
+
+        }
+ */
+//Nested loops number pyramid
+        
+
+
+
+    }
+}
