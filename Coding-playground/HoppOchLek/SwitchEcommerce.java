77fb75a2387f96c20b1c0e81352c55c9a9b13091
ThereseBulai <therese.bulai@gmail.com>
2024-09-15 10:39:22 +0200

    started with assignment

--- Coding-playground/HoppOchLek/SwitchEcommerce.java
+++ Coding-playground/HoppOchLek/SwitchEcommerce.java
@@ -1,41 +1,38 @@
 import java.util.Scanner;
 
 public class SwitchEcommerce {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("Chose product:");
     System.out.println("1.Electronics. 2. Furniture. 3.Groceries and  4.others");
-    
+
     int products = scanner.nextInt();
+    Scanner s = new Scanner(System.in);
+    switch (products) {
+  
 
-    switch(products) {
-        System.out.println("Chose amount");
-
-          case 1:
-            System.out.println("Chose amount");
-            int products = scanner.nextInt();
-
-            break;
-    }
-          /*   
-            case 2:
-            System.out.println("Settings menu");
-            break;
-            case 3:
-            System.out.println("Exit game");
-            break;
+      case 1:
+        System.out.println("Elctronics - Chose quantity");
+        int amount = s.nextInt();
+      
+        break;
+      case 2:
+        System.out.println("Furnitute - Chose quantity");
+        
+        break;
+      case 3:
+        System.out.println("Groceries - Chose quantity");
        
-*/
-
-      /* System.out.println("Chose amout");
-    
-       int products = scanner.nextInt();
-     
-      */ 
+        break;
+      case 4:
+        System.out.println(" Others - Chose quantity");
+        
+        break;
+    }
 
     scanner.close();
-}
+  }
 
 }
