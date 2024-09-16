103ea05a66d5b11fd0badff18152292df9b6bad1
ThereseBulai <therese.bulai@gmail.com>
2024-09-16 15:09:51 +0200

    finished the assigment

--- Coding-playground/HoppOchLek/Student.java
+++ Coding-playground/HoppOchLek/Student.java
@@ -3,5 +3,26 @@ public class Student {
     int age;
     double grade;
 
+    Student() {
+        this.name = "Maja";
+        this.age = 10;
+        this.grade = 8;
+     }
     
+    public static void main (String [] args){
+        Student elev = new Student();
+        elev.name = "Kalle-hopp";
+        elev.age = 8;
+        elev.grade = 10;
+      
+        Student elev2 = new Student();
+
+        System.out.println(elev.name);
+        System.out.println(elev.age);
+        System.out.println(elev.grade);
+
+        System.out.println(elev2.name);
+        System.out.println(elev2.age);
+        System.out.println(elev2.grade);
+    }
 }
