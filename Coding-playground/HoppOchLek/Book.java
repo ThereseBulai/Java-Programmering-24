6c8092716083c1a7b8efd186b169851408e94bb8
ThereseBulai <therese.bulai@gmail.com>
2024-09-16 11:38:02 +0200

    started with E3

---
+++ Coding-playground/HoppOchLek/Book.java
@@ -0,0 +1,15 @@
+public E3{public static void main(String[]args);{
+
+public class Book {
+
+       String title;
+       String author;
+       int year;
+    }
+
+   Book bok = new Book();
+    
+}
+
+
+}
