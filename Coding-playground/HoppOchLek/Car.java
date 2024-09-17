7e54b8a84f2254ede3da483f20474b4f994e79ca
ThereseBulai <therese.bulai@gmail.com>
2024-09-17 11:14:44 +0200

    finished the assigment

--- Coding-playground/HoppOchLek/Car.java
+++ Coding-playground/HoppOchLek/Car.java
@@ -1,14 +1,53 @@
 public class Car {
-    
+
     String make;
     String model;
     String colour;
     int year;
 
-
-    Car (){
-    
-    
+    // nedan är tre constructors
+    Car(String make, String model, String colour, int year) {
+        this.make = make;
+        this.model = model;
+        this.colour = colour;
+        this.year = year;
 
     }
+
+    Car (String make, String model){
+       this.make = make;
+       this.model = model;
+       this.year = year;
+    }
+
+    Car (String model){
+    this.make = make;
+    }
+
+
+    public void printDetails(){
+        System.out.println("Make " + make);
+        System.out.println("Model " + model);
+        System.out.println("Colour " + colour);
+        System.out.println("Year " + year);
+
+
+
+    }
+    
+    public static void main (String [] args){
+        Car car1 = new Car("Volvo", "240", "Red", 1990);
+        car1.printDetails();
+
+
+        Car car2 = new Car("Toyota", "Yaris", "Unknown", 2024);
+        car2.printDetails();
+
+
+        Car car3 = new Car("Mazda");
+        car3.printDetails();
+
+
+    }
+
 }
