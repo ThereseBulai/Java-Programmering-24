a6d61b44c545721ee924bcddc88b314b6e456957
ThereseBulai <therese.bulai@gmail.com>
2024-09-20 14:53:50 +0200

    not fully done with this

--- Coding-playground/HoppOchLek/Constructors/Books.java
+++ Coding-playground/HoppOchLek/Constructors/Books.java
@@ -5,36 +5,31 @@ public class Books {
     public String author;
     public int year;
 
-    //default constructor
-    public Books(){
+    // default constructor
+    public Books() {
         this("Unknown", "Unknown");
     }
 
-    public Books(String title, String author){
+    public Books(String title, String author) {
         this(title, "Unkown", 1);
 
     }
 
-    public Books(String title, String author, int year){
+    public Books(String title, String author, int year) {
         this.title = title;
         this.author = author;
         this.year = year;
     }
 
-
     public static void main(String[] args) {
-        
+
         Books lotr = new Books("Lord of the rings", "JRR Tolkien", 1960);
-        Books witcher = new Books("Witcher","Unknown", 1995);
-
-        System.out.println(lotr.title);
-        System.out.println(witcher.title);
-
+        Books witcher = new Books("Witcher", "Unknown", 1995);
+        printValues(lotr);
     }
 
-
-
-
+    public static void printValues( Books) {
+        System.out.println();
     }
 
-
+}
