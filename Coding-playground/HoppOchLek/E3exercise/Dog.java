b7fb72a116c45a97961c6412cd6bffad8121f7bd
ThereseBulai <therese.bulai@gmail.com>
2024-09-20 12:01:26 +0200

    created for exersice E3

---
+++ Coding-playground/HoppOchLek/E3exercise/Dog.java
@@ -0,0 +1,19 @@
+package E3exercise;
+
+public class Dog  {
+    public String breed;
+    public String colour;
+    public int age; 
+
+   /* 
+    //default constructor
+    public Dog(){
+        this("Unknown", "Unknown", 1);
+    }
+
+    public Dog()
+
+    */
+}
+
+
---
+++ Coding-playground/HoppOchLek/E3exercise/Dogs.java
@@ -0,0 +1,10 @@
+package E3exercise;
+
+public class Dogs {
+    String name = "Hundarna";
+
+    Dog kai = new Dog();
+    Dog kendji = new Dog();
+
+}
+
