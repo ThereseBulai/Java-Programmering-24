a19c8640142ca53f42e50419633bddec1eb962fc
ThereseBulai <therese.bulai@gmail.com>
2024-09-23 19:37:12 +0200

    created for exercise

---
+++ Codingplayground/E4/Rectangle.java
@@ -0,0 +1,12 @@
+package Codingplayground.E4;
+
+public class Rectangle {
+    void rectagleTo() {
+        int width = 8;
+        int height = 6;
+        int area2 = height * width;
+        int peremiter = height * 2 + width * 2;
+
+        
+    }
+}
