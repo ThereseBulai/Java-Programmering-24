9015d40bc036dd53dad15a95ad29eee0c0fd8059
ThereseBulai <therese.bulai@gmail.com>
2024-09-23 19:36:40 +0200

    created for exercise

---
+++ Codingplayground/E4/Circle.java
@@ -0,0 +1,11 @@
+package Codingplayground.E4;
+
+public class Circle {
+
+    void circleTo() {
+        int width = 4;
+        double circumference = Math.pow((width / 2), 2) * 3.14;
+        double area1 = width / 2 * 3.14;
+
+    }
+}
---
+++ Codingplayground/E4/GeometryTester.java
@@ -0,0 +1,18 @@
+package Codingplayground.E4;
+
+public class GeometryTester {
+    
+    Circle c1 = new Circle();
+    Rectangle r1 = new Rectangle();
+
+   public static void main(String[] args) {
+
+  System.out.println(area);
+    
+   
+
+   
+   }         
+
+}
+
