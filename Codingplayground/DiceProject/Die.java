84236ed9edc566c0075be66e0c3082527af62c7f
ThereseBulai <therese.bulai@gmail.com>
2024-09-26 20:58:02 +0200

    not finished but added some methods and stuff

--- Codingplayground/DiceProject/Die.java
+++ Codingplayground/DiceProject/Die.java
@@ -1,7 +1,35 @@
 package Codingplayground.DiceProject;
 
+import java.util.Random; //måste använda klassen Random för detta... huh! vad menas?
+
 public class Die {
-  
-    //constructor
-    
+    public int dieSides;
+    public int dieRandomRoll;
+
+    // konstruktor antal sidor
+    public Die(int dieSides) {
+        dieSides = 6;
+    }
+
+    // getter för konstruktorn
+    public int getdieSides() {
+        return dieSides;
+    }
+
+    // metod för nuvarande värde, men ska nog bytas ut till metoden längst ner, ällä?
+    public int roll() {
+        int dieRandomRoll = 1;
+        return dieRandomRoll;
+    }
+
+    // getter för metoden
+    public int getdieRandomRoll() {
+    return dieRandomRoll;
+    }
+
+    //nedan för är metoden för att returnera ett slumpmässigt heltal
+    public int nextInt(int maxValue){
+        return dieRandomRoll;
+    }
+
 }
